a565bd803a1398076ee17e04b74f2bd07ca894ae
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-09 23:48:09 -0300

    refactor: retirada tipagem

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,17 +1,26 @@
-import { useSession } from "next-auth/react"
-import Router from "next/router"
-import { useEffect } from "react"
+import Layout from '@/components/base/Layout';
+import TableUser from '@/components/Tables/TableUser';
+import { AuthContext } from '@/_contexts/authContext';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 
 const Users = () => {
-    const {status, data: session} = useSession()
-  
-    useEffect(() => {
-      if(status === "unauthenticated") {
-        Router.replace('/Login')
-      }
-    },[status])
+    const router = useRouter();
+    const { isAuthenticated } = useContext(AuthContext);
 
-    return <div>Users</div>
-}
+    useEffect(() => {
+        if (!isAuthenticated) {
+            router.replace('/login');
+        }
+    }, [isAuthenticated, router]);
+
+    return (
+        <Layout>
+            <main>
+                <TableUser />
+            </main>
+        </Layout>
+    );
+};
 
 export default Users;
