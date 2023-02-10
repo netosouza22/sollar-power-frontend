81317079fae31e978d12614c718fec9b17f12831
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-09 23:28:06 -0300

    feat: adicionado toastContainer para que ele possa ser acessado em qualquer instancia

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.scss';
+import { AuthProvider } from '@/_contexts/authContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <SessionProvider session={pageProps.session}>
+        <AuthProvider>
+            <ToastContainer />
             <Component {...pageProps} />
-        </SessionProvider>
+        </AuthProvider>
     );
 }
