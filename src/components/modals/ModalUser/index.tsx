201d29dda4bf39c5a8fbdc6d117a3fba3c6e755c
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-09 23:46:39 -0300

    feat: corrigido erro de nao aparecer a senha no modal de usuarios

--- src/components/modals/ModalUser/index.tsx
+++ src/components/modals/ModalUser/index.tsx
@@ -8,7 +8,7 @@ import { FocusEvent, useContext, useEffect } from 'react';
 import { Col, Container, Form, Modal, Row } from 'react-bootstrap';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { IMaskInput } from 'react-imask';
-import { toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import * as yup from 'yup';
 
 interface ModalUser {
@@ -136,159 +136,181 @@ const ModalUser = ({ showModal, hideModal, handleReload, id, accessType }: Modal
                     }
                 })
                 .catch((err) => {
-                    toast.error('Ocorreu um erro ao criar o usuário!', {
-                        position: toast.POSITION.TOP_RIGHT,
-                    });
+                    if (err?.message) {
+                        toast.error(`${err.message}`, {
+                            position: toast.POSITION.TOP_RIGHT,
+                        });
+                    }
                 });
         }
     };
 
     return (
-        <Modal show={showModal} onHide={handleCloseModal} size="lg">
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Adicionar Usuário</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Container className="my-3">
-                        <Row>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="name">Nome</Form.Label>
+        <>
+            <ToastContainer />
+            <Modal show={showModal} onHide={handleCloseModal} size="lg">
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Adicionar Usuário</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container className="my-3">
+                            <Row>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="name">Nome</Form.Label>
 
-                                <Form.Control {...register('name')} className={`${errors.name ? 'is-invalid' : ''}`} type="text" id="name" aria-describedby="passwordHelpBlock" />
-                            </Col>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="cpf">CPF</Form.Label>
-                                <Controller
-                                    control={control}
-                                    name="cpf"
-                                    render={({ field: { onChange, onBlur, value, name, ref } }) => (
-                                        <Form.Control
-                                            as={IMaskInput}
-                                            mask="000.000.000-00"
-                                            className={`${errors.cpf ? 'is-invalid' : ''}`}
-                                            onChange={onChange}
-                                            value={value}
-                                            type="text"
-                                            id="cpf"
-                                            aria-describedby="passwordHelpBlock"
-                                        />
-                                    )}
-                                />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="email">Email</Form.Label>
-                                <Form.Control {...register('email')} className={`${errors.email ? 'is-invalid' : ''}`} autoComplete="off" type="email" id="email" aria-describedby="email" />
-                            </Col>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="client_cellphone">N° do Cliente</Form.Label>
+                                    <Form.Control {...register('name')} className={`${errors.name ? 'is-invalid' : ''}`} type="text" id="name" aria-describedby="passwordHelpBlock" />
+                                </Col>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="cpf">CPF</Form.Label>
+                                    <Controller
+                                        control={control}
+                                        name="cpf"
+                                        render={({ field: { onChange, onBlur, value, name, ref } }) => (
+                                            <Form.Control
+                                                as={IMaskInput}
+                                                mask="000.000.000-00"
+                                                className={`${errors.cpf ? 'is-invalid' : ''}`}
+                                                onChange={onChange}
+                                                value={value}
+                                                type="text"
+                                                id="cpf"
+                                                aria-describedby="passwordHelpBlock"
+                                            />
+                                        )}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="email">Email</Form.Label>
+                                    <Form.Control {...register('email')} className={`${errors.email ? 'is-invalid' : ''}`} autoComplete="off" type="email" id="email" aria-describedby="email" />
+                                </Col>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="client_cellphone">N° do Cliente</Form.Label>
 
-                                <Controller
-                                    control={control}
-                                    name="cellphone"
-                                    render={({ field: { onChange, onBlur, value, name, ref }, fieldState: { invalid, isTouched, isDirty, error }, formState }) => (
+                                    <Controller
+                                        control={control}
+                                        name="cellphone"
+                                        render={({ field: { onChange, onBlur, value, name, ref }, fieldState: { invalid, isTouched, isDirty, error }, formState }) => (
+                                            <Form.Control
+                                                as={IMaskInput}
+                                                mask="(00) 00000-0000"
+                                                value={value}
+                                                className={`${errors.cellphone ? 'is-invalid' : ''}`}
+                                                onChange={onChange}
+                                                type="text"
+                                                id="cellphone"
+                                                aria-describedby="passwordHelpBlock"
+                                            />
+                                        )}
+                                    />
+                                </Col>
+
+                                {accessType !== 'edit' && (
+                                    <Col className="my-1">
+                                        <Form.Label htmlFor="password">Password</Form.Label>
                                         <Form.Control
-                                            as={IMaskInput}
-                                            mask="(00) 00000-0000"
-                                            value={value}
-                                            className={`${errors.cellphone ? 'is-invalid' : ''}`}
-                                            onChange={onChange}
-                                            type="text"
-                                            id="cellphone"
-                                            aria-describedby="passwordHelpBlock"
+                                            {...register('password')}
+                                            className={`${errors.password ? 'is-invalid' : ''}`}
+                                            autoComplete="off"
+                                            type="password"
+                                            id="email"
+                                            aria-describedby="email"
                                         />
-                                    )}
-                                />
-                            </Col>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="password">Password</Form.Label>
-                                <Form.Control {...register('password')} className={`${errors.password ? 'is-invalid' : ''}`} autoComplete="off" type="password" id="email" aria-describedby="email" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="my-1" xs={3}>
-                                <Form.Label htmlFor="cep">CEP</Form.Label>
-                                <Controller
-                                    control={control}
-                                    name="cep"
-                                    render={({ field: { onChange, onBlur, value, name, ref } }) => (
-                                        <Form.Control
-                                            as={IMaskInput}
-                                            mask="00.000-000"
-                                            className={`${errors.cep ? 'is-invalid' : ''}`}
-                                            onChange={onChange}
-                                            onBlur={(e) => handleCepInformation(e)}
-                                            type="text"
-                                            value={value}
-                                            id="client_cellphone"
-                                            aria-describedby="passwordHelpBlock"
-                                        />
-                                    )}
-                                />
-                            </Col>
-                            <Col className="my-1" xs={3}>
-                                <Form.Label htmlFor="state">Estado</Form.Label>
-                                <Form.Select {...register('state')} className={`${errors.state ? 'is-invalid' : ''}`} autoComplete="off" id="state" aria-describedby="passwordHelpBlock">
-                                    {states.map((state: any) => {
-                                        return (
-                                            <option key={state.value} value={state.value}>
-                                                {state.label}
-                                            </option>
-                                        );
-                                    })}
-                                </Form.Select>
-                            </Col>
-                            <Col className="my-1" xs={6}>
-                                <Form.Label htmlFor="city">Cidade</Form.Label>
-                                <Form.Control {...register('city')} autoComplete="off" className={`${errors.city ? 'is-invalid' : ''}`} type="text" id="city" aria-describedby="passwordHelpBlock" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="neighborhood">Bairro</Form.Label>
-                                <Form.Control
-                                    {...register('neighborhood')}
-                                    className={`${errors.neighborhood ? 'is-invalid' : ''}`}
-                                    autoComplete="off"
-                                    type="text"
-                                    id="neighborhood"
-                                    aria-describedby="passwordHelpBlock"
-                                />
-                            </Col>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="address">Rua</Form.Label>
-                                <Form.Control
-                                    {...register('address')}
-                                    className={`${errors.address ? 'is-invalid' : ''}`}
-                                    autoComplete="off"
-                                    type="text"
-                                    id="address"
-                                    aria-describedby="passwordHelpBlock"
-                                />
-                            </Col>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="number">Número</Form.Label>
-                                <Form.Control
-                                    {...register('number')}
-                                    className={`${errors.number ? 'is-invalid' : ''}`}
-                                    autoComplete="off"
-                                    type="number"
-                                    id="number"
-                                    aria-describedby="passwordHelpBlock"
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button type="submit" size="lg">
-                        Salvar
-                    </Button>
-                </Modal.Footer>
-            </Form>
-        </Modal>
+                                    </Col>
+                                )}
+                            </Row>
+                            <Row>
+                                <Col className="my-1" xs={3}>
+                                    <Form.Label htmlFor="cep">CEP</Form.Label>
+                                    <Controller
+                                        control={control}
+                                        name="cep"
+                                        render={({ field: { onChange, onBlur, value, name, ref } }) => (
+                                            <Form.Control
+                                                as={IMaskInput}
+                                                mask="00.000-000"
+                                                className={`${errors.cep ? 'is-invalid' : ''}`}
+                                                onChange={onChange}
+                                                onBlur={(e) => handleCepInformation(e)}
+                                                type="text"
+                                                value={value}
+                                                id="client_cellphone"
+                                                aria-describedby="passwordHelpBlock"
+                                            />
+                                        )}
+                                    />
+                                </Col>
+                                <Col className="my-1" xs={3}>
+                                    <Form.Label htmlFor="state">Estado</Form.Label>
+                                    <Form.Select {...register('state')} className={`${errors.state ? 'is-invalid' : ''}`} autoComplete="off" id="state" aria-describedby="passwordHelpBlock">
+                                        {states.map((state: any) => {
+                                            return (
+                                                <option key={state.value} value={state.value}>
+                                                    {state.label}
+                                                </option>
+                                            );
+                                        })}
+                                    </Form.Select>
+                                </Col>
+                                <Col className="my-1" xs={6}>
+                                    <Form.Label htmlFor="city">Cidade</Form.Label>
+                                    <Form.Control
+                                        {...register('city')}
+                                        autoComplete="off"
+                                        className={`${errors.city ? 'is-invalid' : ''}`}
+                                        type="text"
+                                        id="city"
+                                        aria-describedby="passwordHelpBlock"
+                                    />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="neighborhood">Bairro</Form.Label>
+                                    <Form.Control
+                                        {...register('neighborhood')}
+                                        className={`${errors.neighborhood ? 'is-invalid' : ''}`}
+                                        autoComplete="off"
+                                        type="text"
+                                        id="neighborhood"
+                                        aria-describedby="passwordHelpBlock"
+                                    />
+                                </Col>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="address">Rua</Form.Label>
+                                    <Form.Control
+                                        {...register('address')}
+                                        className={`${errors.address ? 'is-invalid' : ''}`}
+                                        autoComplete="off"
+                                        type="text"
+                                        id="address"
+                                        aria-describedby="passwordHelpBlock"
+                                    />
+                                </Col>
+                                <Col className="my-1">
+                                    <Form.Label htmlFor="number">Número</Form.Label>
+                                    <Form.Control
+                                        {...register('number')}
+                                        className={`${errors.number ? 'is-invalid' : ''}`}
+                                        autoComplete="off"
+                                        type="number"
+                                        id="number"
+                                        aria-describedby="passwordHelpBlock"
+                                    />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" size="lg">
+                            Salvar
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
+        </>
     );
 };
 
