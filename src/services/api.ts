c10c9fd4342f5ae058c3f1f245bf5b5b2ad85107
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-13 15:08:24 -0300

    refactor: retirada de linha inutil e de código comentado

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,14 +7,12 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        //'Authorization': 'token <your-token-here> -- https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
       }
 })
 
 if(auth_token){
     api.defaults.headers['Authorization'] = `Bearer ${auth_token}`;
 }
-
 
 export { api };
 
