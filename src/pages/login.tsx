17a16a1f1a40e1e2b4bca507e0c81753f67c476e
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-13 11:33:17 -0300

    feat: adicionado texto header com logo e texto  ao container do login, retirado barra de menu do top da tela, centralizado schemas e types em um arquivo na pasta utils

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,18 @@
+import LogoSollarPower from '@/assets/sollar-power.png';
 import Button from '@/components/base/Button';
-import Header from '@/components/base/Header';
 import styles from '@/styles/pages/login.module.scss';
 import { AuthContext } from '@/_contexts/authContext';
 import { yupResolver } from '@hookform/resolvers/yup';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
-import * as yup from 'yup';
 
-const schema = yup
-    .object({
-        email: yup.string().email().required('Campo Obrigatório!'),
-        password: yup.string().required('Campo Obrigatório!'),
-    })
-    .required();
+import { schemaLogin } from '@/utils/schemas';
 
-type FormDataLogin = yup.InferType<typeof schema>;
+import { FormDataLogin } from '@/@types/forms';
 
 const Login = () => {
     const router = useRouter();
@@ -29,16 +24,17 @@ const Login = () => {
         handleSubmit,
         formState: { errors },
     } = useForm<FormDataLogin>({
-        resolver: yupResolver(schema),
+        resolver: yupResolver(schemaLogin),
     });
 
     useEffect(() => {
         const verifyAccess = () => {
             if (isAuthenticated) {
+                setIsLoading(false);
                 router.push('/home');
             } else {
-                router.push('/login');
                 setIsLoading(false);
+                router.push('/');
             }
         };
 
@@ -61,40 +57,51 @@ const Login = () => {
     }
 
     return (
-        <>
-            <Header />
-
+        <div className={styles.login__page}>
             <div className={styles.login__container}>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Container>
-                        <Row>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="email">Email</Form.Label>
-                                <Form.Control {...register('email')} className={`${errors.email ? 'is-invalid' : ''}`} autoComplete="off" type="email" id="email" aria-describedby="email do usuário" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="my-1">
-                                <Form.Label htmlFor="password">Senha</Form.Label>
-                                <Form.Control
-                                    {...register('password')}
-                                    className={`${errors.password ? 'is-invalid' : ''}`}
-                                    autoComplete="off"
-                                    type="password"
-                                    id="password"
-                                    aria-describedby="senha do usuário"
-                                />
-                            </Col>
-                        </Row>
-                        <Row className="d-flex justify-content-center mt-4">
-                            <Button type="submit" size="lg">
-                                Acessar
-                            </Button>
-                        </Row>
-                    </Container>
-                </Form>
+                <header className="d-flex w-100 justify-content-center align-items-center flex-column">
+                    <Image src={LogoSollarPower} alt="system logo" width={200}></Image>
+                    <h4 className={styles.login__text_info}>Bem vindo ao Sollar Power!</h4>
+                </header>
+                <main>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Container>
+                            <Row>
+                                <Col className="my-1" xs={12}>
+                                    <Form.Label htmlFor="email">Email</Form.Label>
+                                    <Form.Control
+                                        {...register('email')}
+                                        className={`${errors.email ? 'is-invalid' : ''}`}
+                                        autoComplete="off"
+                                        type="email"
+                                        id="email"
+                                        aria-describedby="email do usuário"
+                                    />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="my-1" xs={12}>
+                                    <Form.Label htmlFor="password">Senha</Form.Label>
+                                    <Form.Control
+                                        {...register('password')}
+                                        className={`${errors.password ? 'is-invalid' : ''}`}
+                                        autoComplete="off"
+                                        type="password"
+                                        id="password"
+                                        aria-describedby="senha do usuário"
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="d-flex justify-content-center mt-4">
+                                <Button type="submit" size="lg">
+                                    Acessar
+                                </Button>
+                            </Row>
+                        </Container>
+                    </Form>
+                </main>
             </div>
-        </>
+        </div>
     );
 };
 
