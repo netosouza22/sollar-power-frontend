1ea000451b85ab31b992a8845925d36a23052281
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-14 11:06:02 -0300

    feat: adicionado arquivo _document.tsx para definir fonts no header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    console.log('herer');
-
     return (
-        <Html lang="en">
-            <Head />
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
