ed3649c7ce6839d1ea970ecca6dbbd2fce393705
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-14 15:04:47 -0300

    feat: adicionado link a logo para que o cliente possa ao clicar na logo voltar para a home

--- src/components/base/Header/index.tsx
+++ src/components/base/Header/index.tsx
@@ -2,48 +2,38 @@ import LogoImg from '@/assets/sollar-power.png';
 import { AuthContext } from '@/_contexts/authContext';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { useContext } from 'react';
 import Button from '../Button';
 import styles from './header.module.scss';
 
 const Header = () => {
-    const router = useRouter();
-    const { logout, isAuthenticated } = useContext(AuthContext);
-
-    // const signOut = () => {
-    //     const { auth_token } = parseCookies();
-    //     if (!!auth_token) {
-    //         destroyCookie(null, 'auth_token');
-    //         router.push('/login');
-    //     }
-
-    //     router.push('/login');
-    // };
+    // const router = useRouter();
+    const { logout } = useContext(AuthContext);
 
     return (
         <header className={styles.header}>
             <div className={styles.header__logo}>
-                <Image src={LogoImg} alt="Logo Image" width={150} />
+                <Link href="/home">
+                    <Image src={LogoImg} alt="Logo Image" width={150} />
+                </Link>
             </div>
-            {isAuthenticated && (
-                <nav className={styles.header__nav}>
-                    <ul>
-                        <li>
-                            <Link href="/home">Projetos</Link>
-                        </li>
-                        <li>
-                            <Link href="/users">Usuários</Link>
-                        </li>
 
-                        <li>
-                            <Button bg="" size="lg" onClick={logout}>
-                                Logout
-                            </Button>
-                        </li>
-                    </ul>
-                </nav>
-            )}
+            <nav className={styles.header__nav}>
+                <ul>
+                    <li>
+                        <Link href="/home">Projetos</Link>
+                    </li>
+                    <li>
+                        <Link href="/users">Usuários</Link>
+                    </li>
+
+                    <li>
+                        <Button bg="" size="lg" onClick={logout}>
+                            Logout
+                        </Button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
