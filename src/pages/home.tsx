08c3feb147de4c79ab50325dff730f2a89d72bb0
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-14 11:04:29 -0300

    feat: usando getServerSideProps para redirecionar usuario caso autorizado

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,46 +1,48 @@
 import Layout from '@/components/base/Layout';
 import TableProject from '@/components/Tables/TableProject';
-import { AuthContext } from '@/_contexts/authContext';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-import { useContext, useEffect, useState } from 'react';
+import { parseCookies } from 'nookies';
 
-type Project = {
-    client_cellphone: string;
-    client_name: string;
-    total_potency: number;
-    company_distribution: string;
-    file: string;
+// type Project = {
+//     client_cellphone: string;
+//     client_name: string;
+//     total_potency: number;
+//     company_distribution: string;
+//     file: string;
 
-    cep: string;
-    state: string;
-    city: string;
-    neighborhood: string;
-    address: string;
-    number: number;
-};
+//     cep: string;
+//     state: string;
+//     city: string;
+//     neighborhood: string;
+//     address: string;
+//     number: number;
+// };
 
-const Home = () => {
+const Home = (props: any) => {
+    // console.log('🚀 ~ file: home.tsx:25 ~ Home ~ props', JSON.stringify(props.data));
     const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true);
-    const { isAuthenticated } = useContext(AuthContext);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const { isAuthenticated } = useContext(AuthContext);
 
-    useEffect(() => {
-        const verifyAccess = () => {
-            if (isAuthenticated) {
-                router.push('/home');
-                setIsLoading(false);
-            } else {
-                router.push('/login');
-            }
-        };
+    // useEffect(() => {
+    //     const verifyAccess = () => {
+    //         if (isAuthenticated) {
+    //             router.replace(router.pathname);
+    //             setIsLoading(false);
+    //         } else {
+    //             router.push('/');
+    //         }
+    //     };
 
-        verifyAccess();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isAuthenticated]);
+    //     verifyAccess();
 
-    if (isLoading) {
-        return <div></div>;
-    }
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [isAuthenticated]);
+
+    // if (isLoading) {
+    //     return <div style={{ backgroundColor: 'red', width: '50%' }}>\\\///</div>;
+    // }
 
     return (
         <Layout>
@@ -52,3 +54,19 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { auth_token } = parseCookies(ctx);
+
+    if (!auth_token) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+    return {
+        props: {},
+    };
+};
