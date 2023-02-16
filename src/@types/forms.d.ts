f13a6c9c67565c5df0cefa18afdd19c1401fe348
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-16 15:48:28 -0300

    feat: adicionado types de cada formulários no arquivo forms.d.ts

--- src/@types/forms.d.ts
+++ src/@types/forms.d.ts
@@ -1,3 +1,6 @@
-import { schemaLogin } from "@/utils/schemas";
+import { schemaLogin, schemaProject, schemaUser } from "@/utils/schemas";
+import * as yup from 'yup';
 
-type FormDataLogin = yup.InferType<typeof schemaLogin>;
+export type FormDataLogin = yup.InferType<typeof schemaLogin>;
+export type FormDataUser = yup.InferType<typeof schemaUser>;
+export type FormDataProject = yup.InferType<typeof schemaProject>;
