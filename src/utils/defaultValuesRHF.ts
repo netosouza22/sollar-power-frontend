da8e7434e5cda9733a6c82a1a25e3093e44428d8
Antônio Assis de Sousa Neto <netosouzacp@gmail.com>
2023-02-16 15:47:26 -0300

    feat: criado arquivo que contem todas os valores iniciais dos formulários, necessário para inicializar os valores no react-hook-form

---
+++ src/utils/defaultValuesRHF.ts
@@ -0,0 +1,28 @@
+export const defaultProjectValues = {
+    client_cellphone: '',
+    client_name: '',
+    company_distribution: '',
+    total_potency: 0,
+    cep: '',
+    state: '',
+    city: '',
+    neighborhood: '',
+    address: '',
+    number: 0,
+    file: '',
+};
+
+export const defaultUserValues = {
+    isCreate: false,
+    name: '',
+    cpf: '',
+    email: '',
+    cellphone: '',
+    cep: '',
+    password: '',
+    state: '',
+    city: '',
+    neighborhood: '',
+    address: '',
+    number: 0,
+};
